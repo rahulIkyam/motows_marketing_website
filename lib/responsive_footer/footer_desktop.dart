51a5451a3a37079a3d45572ae26d753e785632f2
Rahul <rahul.nc@ikyam.com>
2023-06-21 10:56:11 +0530

    Footer desktop view, fixed the issue

--- lib/responsive_footer/footer_desktop.dart
+++ lib/responsive_footer/footer_desktop.dart
@@ -25,61 +25,123 @@ class _FooterDesktopState extends State<FooterDesktop> {
             child: Center(
               child: Image.asset(
                 "assets/images/rectangle1.png",
-                height: 450,
+                height: 500,
                 fit: BoxFit.fill,
               ),
             ),
           ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Expanded(
+          //       child: Column(
+          //         crossAxisAlignment: CrossAxisAlignment.start,
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: [
+          //           const Padding(
+          //             padding: EdgeInsets.only(left: 200),
+          //             child: Text(
+          //               "Download Our Workshop \nOwner App Today",
+          //               maxLines: 5,
+          //               style: TextStyle(
+          //                   fontSize: 40,
+          //                   fontWeight: FontWeight.bold,
+          //                   height: 1.5,
+          //                   color: Colors.white),
+          //             ),
+          //           ),
+          //           const Padding(
+          //             padding: EdgeInsets.only(left: 200),
+          //             child: Text(
+          //               "Take control of your workshop's success with our \npowerful and user-friendly mobile app.",
+          //               maxLines: 5,
+          //               style: TextStyle(
+          //                   fontSize: 20,
+          //                   fontWeight: FontWeight.w500,
+          //                   height: 1.5,
+          //                   color: Colors.white),
+          //             ),
+          //           ),
+          //           Padding(
+          //             padding: const EdgeInsets.only(left: 190),
+          //             child: Image.asset(
+          //               "assets/images/google_play.png",
+          //               fit: BoxFit.fill,
+          //               height: 60,
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //     Padding(
+          //       padding: const EdgeInsets.only(right: 250,),
+          //       child: Image.asset(
+          //         "assets/images/dashboard.png",
+          //         fit: BoxFit.fill,
+          //         height: 370,
+          //       ),
+          //     ),
+          //   ],
+          // ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 200),
-                      child: Text(
-                        "Download Our Workshop \nOwner App Today",
-                        maxLines: 5,
-                        style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            height: 1.5,
-                            color: Colors.white),
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 100, left: 250),
+                  child: Container(
+                    height: 400,
+                    color: Colors.transparent,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            "Download Our Workshop \nOwner App Today",
+                            maxLines: 5,
+                            style: TextStyle(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                height: 1.5,
+                                color: Colors.white),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            "Take control of your workshop's success with our \npowerful and user-friendly mobile app.",
+                            maxLines: 5,
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                height: 1.5,
+                                color: Colors.white),
+                          ),
+                        ),
+                        Image.asset(
+                          "assets/images/google_play.png",
+                          fit: BoxFit.fill,
+                          height: 60,
+                        ),
+                      ],
                     ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 200),
-                      child: Text(
-                        "Take control of your workshop's success with our \npowerful and user-friendly mobile app.",
-                        maxLines: 5,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            height: 1.5,
-                            color: Colors.white),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 190),
-                      child: Image.asset(
-                        "assets/images/google_play.png",
-                        fit: BoxFit.fill,
-                        height: 60,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(right: 250,),
-                child: Image.asset(
-                  "assets/images/dashboard.png",
-                  fit: BoxFit.fill,
-                  height: 370,
+                padding: const EdgeInsets.only(top: 150, right: 260),
+                child: Container(
+                  height: 280,
+                  color: Colors.transparent,
+                  child: Image.asset(
+                    "assets/images/dashboard.png",
+                    fit: BoxFit.fill,
+                    height: 300,
+                  ),
                 ),
               ),
             ],
